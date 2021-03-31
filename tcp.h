2142960b48c5ec065b41f3e21ac50651d29085a3
ocfbnj <ocfbnj@qq.com>
2021-03-31 20:40:15 +0800

    rename type to method

--- tcp.h
+++ tcp.h
@@ -6,9 +6,9 @@
 
 #include "AEAD.h"
 
-asio::awaitable<void> tcpRemote(AEAD::Method type, std::string_view remotePort, std::string_view password);
+asio::awaitable<void> tcpRemote(AEAD::Method method, std::string_view remotePort, std::string_view password);
 
-asio::awaitable<void> tcpLocal(AEAD::Method type,
+asio::awaitable<void> tcpLocal(AEAD::Method method,
                                std::string_view remoteHost, std::string_view remotePort,
                                std::string_view localPort,
                                std::string_view password);
