0447617391102f472458cb0f94a4c10c54bbfc20
ocfbnj <ocfbnj@qq.com>
2021-06-16 18:57:25 +0800

    make it possible to compile with MSVC

--- src/AsyncObject.h
+++ src/AsyncObject.h
@@ -5,10 +5,16 @@
 #include <asio/ts/socket.hpp>
 #include <asio/use_awaitable.hpp>
 
+#ifdef WIN32
+using Acceptor = asio::ip::tcp::acceptor;
+using TCPSocket = asio::ip::tcp::socket;
+using Resolver = asio::ip::tcp::resolver;
+#else
 using DefaultToken = asio::use_awaitable_t<>;
 
 using Acceptor = DefaultToken::as_default_on_t<asio::ip::tcp::acceptor>;
 using TCPSocket = DefaultToken::as_default_on_t<asio::ip::tcp::socket>;
 using Resolver = DefaultToken::as_default_on_t<asio::ip::tcp::resolver>;
+#endif
 
 #endif
